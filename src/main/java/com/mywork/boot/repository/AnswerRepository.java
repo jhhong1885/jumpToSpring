00c29552df483afd61f865717dcf1d03c83426b6
hongjihyeog <gurwl1885@gmail.com>
2025-05-13 02:41:06 +0900

    점프 투 스프링부트 question testJpa

--- src/main/java/com/mywork/boot/repository/AnswerRepository.java
+++ src/main/java/com/mywork/boot/repository/AnswerRepository.java
@@ -2,6 +2,8 @@ package com.mywork.boot.repository;
 
 import com.mywork.boot.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 }
--- src/test/java/com/mywork/boot/BootApplicationTests.java
+++ src/test/java/com/mywork/boot/BootApplicationTests.java
@@ -1,13 +1,18 @@
 package com.mywork.boot;
 
+import com.mywork.boot.entity.Answer;
 import com.mywork.boot.entity.Question;
+import com.mywork.boot.repository.AnswerRepository;
 import com.mywork.boot.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -15,13 +20,19 @@ class BootApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Transactional
 	@Test
 	void testJpa(){
 		Optional<Question> optional = this.questionRepository.findById(1);
 		assertTrue(optional.isPresent());
-
 		Question question = optional.get();
-		question.setSubject("hello world");
-		this.questionRepository.save(question);
+
+		List<Answer> answerList = question.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("반갑습니다.", answerList.getFirst().getContent());
 	}
 }
