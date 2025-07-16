e728796fc0b600286969a61d1f7823a8ba29c0d7
hongjihyeog <gurwl1885@gmail.com>
2025-07-16 23:24:55 +0900

    점프 투 스프링부트 로그아웃 기능 추가

--- src/main/java/com/mywork/boot/SecurityConfig.java
+++ src/main/java/com/mywork/boot/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mywork.boot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +21,11 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/user/login")
-                        .defaultSuccessUrl("/"))
+                        .defaultSuccessUrl("/question/list"))
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/question/list")
+                        .invalidateHttpSession(true)) //로그아웃 성공시 세션 삭제
         ;
         return http.build();
     }
@@ -29,5 +35,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
